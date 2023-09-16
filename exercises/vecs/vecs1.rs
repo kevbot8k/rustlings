ba0bbaf1ef9043e5dd1396da3f53b79138e6f743
Kenan Farmer <kenanfarmer@gmail.com>
2023-09-16 16:56:30 -0400

    fix flag

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -7,8 +7,6 @@
 //
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
     let v: Vec<i32> = vec![10, 20, 30, 40]; // more ways to do this with Vec::new() and then pushing from a to v or a.to_vec()
