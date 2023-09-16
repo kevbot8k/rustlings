9034c481499fd9f400a274f458062b3d2ba8ab97
Kenan Farmer <kenanfarmer@gmail.com>
2023-09-16 15:38:34 -0400

    vecs section annotated

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -11,7 +11,11 @@
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let v: Vec<i32> = vec![10, 20, 30, 40]; // more ways to do this with Vec::new() and then pushing from a to v or a.to_vec()
+    let v: Vec<i32> = a.to_vec();
+    let v: Vec<i32> = a.into_iter().collect(); // into_iter iterates by value iter() is by reference and would need to be mapped
+    let v: Vec<i32> = a.iter().map(|x| *x).collect();
+    //vec!(&a[0..]); would be Vec<&[i32]> and vec!a would be Vec<[i32; 4]>
 
     (a, v)
 }
