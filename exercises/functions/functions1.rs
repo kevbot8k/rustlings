0b976db779103bad3516dc59e1668ecbca88d20e
Kenan Farmer <kenanfarmer@gmail.com>
2023-09-16 14:57:23 -0400

    functions section

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,7 +3,11 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// pub fns are for exposing functions publicly for consumption elsewhere. fn are private by default. 
+// for more info on function visbility: https://doc.rust-lang.org/reference/visibility-and-privacy.html 
+fn call_me() {
+
+}
 
 fn main() {
     call_me();
--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -3,13 +3,13 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);
 }
 
+// can either use implicit return like below, or explicit return statement
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num
 }
